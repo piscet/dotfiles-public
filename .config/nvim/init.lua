9254a7f39bd0364b39aed1912b769fa2faf618ab
pisce <pisce@ymail.ne.jp>
2024-08-18 09:29:28 +0900

    hello nvim.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,20 @@
+-- option
+-- - basic
+vim.opt.encoding        = "utf-8"
+vim.opt.fileencoding    = "utf-8"
+-- vim.opt.clipboard       = "unnamed"
+
+vim.opt.cmdheight       = 2
+vim.opt.showcmd         = true
+vim.opt.showmode        = false
+
+-- - view
+vim.opt.number          = true
+
+-- - search
+vim.opt.ignorecase      = true
+vim.opt.smartcase       = true
+vim.opt.wrapscan        = true
+vim.opt.incsearch       = true
+vim.opt.hlsearch        = true
+
