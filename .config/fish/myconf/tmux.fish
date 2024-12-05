27c0d12e46346b9769ab99f28e122a22f5437e43
pisce <pisce@ymail.ne.jp>
2024-12-05 23:38:51 +0900

    tmux assistant.

--- .config/fish/myconf/tmux.fish
+++ .config/fish/myconf/tmux.fish
@@ -1,20 +1,90 @@
-if not status is-interactive
+set _read_cmd "echo 'choose session > '"
+set _new_session_cmd "echo 'session name > '"
+
+if not status is-interactive; \
+   or set -q SSH_CLIENT; \
+   or string length -q -- $TMUX
     exit 0
 end
 
-if set -q SSH_CLIENT ;or string length -q -- $TMUX
-    exit 0
+while true
+    for line in (tmux list-sessions -F '#{session_id} #{?session_attached,yes,no} #S')
+        set _line_split (echo $line | string split ' ')
+        set _session_id     $_session_id     $_line_split[1]
+        set _session_active $_session_active $_line_split[2]
+        set _session_name   $_session_name   $_line_split[3]
+    end
+
+    set _num_of_session (count $_session_id)
+    set _index_list (seq $_num_of_session)
+    printf 'index | active | session_id : session_name\n'
+    printf '------+--------+----------------------------\n'
+    for i in $_index_list
+        printf '%5d | ' $i
+        printf '%6s | ' $_session_active[$i]
+        printf '%s \t: %s\n' $_session_id[$i] $_session_name[$i]
+    end
+
+    printf '\n'
+    printf 'Please select an index number, n or q.\n'
+    printf '  n : create a new session.\n'
+    printf '  q : quit tmux.\n'
+    printf '\n'
+
+    read -p $_read_cmd _select_index
+
+    if not string length -q -- $_select_index
+        break
+    end
+
+    switch $_select_index
+        case n
+            # create new session.
+            while true
+                read -p $_new_session_cmd _new_session_name; 
+
+                if not string length -q -- $_new_session_name
+                    break
+                end
+
+                printf 'create session %s...\n\n' $_new_session_name
+
+                if not tmux list-sessions -F\#S | grep -q $_new_session_name
+                    if not tmux new -s $_new_session_name
+                        printf '\n'
+                        printf 'Error: Failed to create session %s ' $_new_session_name
+                        printf 'due to an error with the tmux command.\n'
+                    end
+
+                    break
+                else
+                    printf 'Error: Failed to create session %s ' $_new_session_name
+                    printf 'due to a duplicate session name.\n'
+                end
+
+                printf 'please specify a different session name.\n'
+                set -e _new_session_name
+            end
+
+            break
+        case q
+            break
+        case $_index_list
+            set _attach_name $_session_name[$_select_index]
+            if not tmux attach-session -t $_attach_name
+                printf '\n'
+                printf 'Error: Failed to attach session %s ' $_attach_name
+                printf 'due to an error with the tmux command.\n'
+            end
+            exit 0
+    end
+
+    # erase variable
+    set -e _session_id
+    set -e _session_active
+    set -e _session_name
+    set -e _select_index
 end
 
-if not set -q SESSION_NAME
-    set -x SESSION_NAME Term
-end
-
-if not tmux list-sessions -F\#S | grep -q $SESSION_NAME
-    tmux new -s $SESSION_NAME
-else
-    tmux new
-end
-
-exit 0
+printf "quit.\n"
 
