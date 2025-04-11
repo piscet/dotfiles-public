ad0b8d953513749da795c6e9a97cef02c43c0ab5
pisce <pisce@ymail.ne.jp>
2025-04-11 23:01:04 +0900

    add alias

---
+++ .config/fish/myconf/conf.d/alias.fish
@@ -0,0 +1,14 @@
+# file
+alias l="ls -CF"
+alias ll="ls -lh"
+alias la="ls -lhA"
+
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -i"
+
+alias g="git"
+
+# dotfiles
+alias dot="cd ~/.dotfiles/dotfiles-public"
+alias pdot="cd ~/.dotfiles"
