abee833872c442146cf6b7555e012c38ae23ebb8
pisce <pisce@ymail.ne.jp>
2025-04-29 23:59:05 +0900

    add neovim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,8 @@
 -- option
 -- - basic
 vim.opt.encoding        = "utf-8"
-vim.opt.fileencoding    = "utf-8"
+vim.opt.fileencodings   = "utf-8,sjis"
+vim.opt.fileformats     = "unix,dos,mac"
 vim.opt.mouse           = ""
 -- vim.opt.clipboard       = "unnamed"
 
@@ -9,13 +10,23 @@ vim.opt.cmdheight       = 2
 vim.opt.showcmd         = true
 vim.opt.showmode        = false
 
+vim.opt.tagstack        = true
+
 -- - view
 vim.opt.number          = true
 vim.opt.title           = true
 vim.opt.wrap            = false
 vim.opt.showtabline     = 2
+
 vim.opt.scrolloff       = 5
 vim.opt.sidescrolloff   = 10
+vim.opt.errorbells      = false
+
+vim.opt.cursorline      = true
+vim.opt.cursorlineopt   = number
+
+vim.opt.display         = "lastline"
+vim.opt.syntax          = "enable"
 
 -- - search
 vim.opt.ignorecase      = true
@@ -30,6 +41,14 @@ vim.opt.expandtab       = true
 vim.opt.shiftwidth      = 4
 vim.opt.smartindent     = true
 vim.opt.cindent         = true
+
+-- - window
 vim.opt.splitbelow      = true
 vim.opt.splitright      = true
+
+-- - complement
+vim.opt.wildmenu        = true
+vim.opt.wildmode        = "full"
+vim.opt.pumheight       = 8
+vim.opt.completeopt     = "menuone,noinsert"
 
