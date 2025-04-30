9cc4d601d1a1c4fd7c62ac8dda14ff6dea0f4c96
pisce <pisce@ymail.ne.jp>
2025-04-30 23:45:34 +0900

    Add aliases to move to config directorys

--- .config/fish/myconf/conf.d/alias.fish
+++ .config/fish/myconf/conf.d/alias.fish
@@ -9,6 +9,14 @@ alias rm="rm -i"
 
 alias g="git"
 
+# editor
+alias vim="nvim"
+
 # dotfiles
 alias dot="cd ~/.dotfiles/dotfiles-public"
 alias pdot="cd ~/.dotfiles"
+
+alias dfish="cd ~/.config/fish/"
+alias dvim="cd ~/.config/nvim/"
+alias tmux-conf="nvim ~/.config/tmux/tmux.conf"
+
