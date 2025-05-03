78729a7d4ef63283d774fce7540c937992af8b9f
pisce <pisce@ymail.ne.jp>
2025-05-03 23:08:24 +0900

    add keybind

--- .config/nvim/lua/plugins/fzf-lua-settings.lua
+++ .config/nvim/lua/plugins/fzf-lua-settings.lua
@@ -1,8 +1,12 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
-local actions = require("fzf-lua").actions
+local fzf = require("fzf-lua")
+local actions = fzf.actions
+local config = fzf.config
 
+config.defaults.keymap.builtin['<A-u>'] = 'preview-page-up'
+config.defaults.keymap.builtin['<A-d>'] = 'preview-page-down'
 
 require("fzf-lua").setup {
     keymap = {
@@ -11,22 +15,15 @@ require("fzf-lua").setup {
             ["ctrl-d"]  = "half-page-down",
             ["ctrl-u"]  = "half-page-up",
             ["ctrl-w"]  = "backward-kill-word",
-            -- ["ctrl-j"]  = "preview-down",
-            -- ["ctrl-k"]  = "preview-up",
+            ["alt-g"]  = "first",
+            ["alt-G"]  = "last",
         },
     },
     actions = {
         files = {
             ["enter"]   = actions.file_tabedit,
-            -- ["ctrl-w"]  = actions.file_split,
-            ["ctrl-v"]  = actions.file_vsplit,
-            -- ["ctrl-w"]  = actions.file_split,
-            -- ["ctrl-v"]  = actions.file_vsplit,
-            -- ["alt-q"]  = actions.file_sel_to_qf,
-            -- ["alt-Q"]  = actions.file_sel_to_ll,
-            -- ["alt-i"]  = { fn = actions.toggle_ignore, reuse = true, header = false },
-            -- ["alt-h"]  = { fn = actions.toggle_hidden, reuse = true, header = false },
-            -- ["alt-f"]  = { fn = actions.toggle_follow, reuse = true, header = false },
+            ["alt-w"]  = actions.file_split,
+            ["alt-v"]  = actions.file_vsplit,
         },
     }
 }
@@ -36,5 +33,3 @@ keymap.set("n", "sf", function() require('fzf-lua').files() end, opts)
 keymap.set("n", "<Leader>t", function() require('fzf-lua').grep_cword() end, opts)
 keymap.set("n", "<Leader>f", function() require('fzf-lua').grep() end, opts)
 
--- 
--- keymap.set("n", "", "<cmd>lua require('fzf-lua').files()<Return>")
