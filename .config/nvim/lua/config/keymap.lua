23ea9ecd399a7f8eff31ffe34d9993352280e1fb
pisce <pisce@ymail.ne.jp>
2025-05-03 16:45:00 +0900

    add option

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,53 +1,74 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
-keymap.set("n", "Y", "y$")
+vim.g.mapleader = " "
 
-keymap.set("n", "s", "<nop>")
+-- basic
+keymap.set("n", "Y", "y$", opts)
+keymap.set("n", "<esc><esc>", ":noh<Return>", opts)
+keymap.set("n", "s", "<nop>", opts)
 
 -- Move tab
-keymap.set("n", "<tab>", ":tabnext<Return>")
-keymap.set("n", "<s-tab>", ":tabprev<Return>")
-keymap.set("n", "ss", ":tabnext<Return>")
-keymap.set("n", "sn", ":tabnext<Return>")
-keymap.set("n", "sp", ":tabprev<Return>")
+-- keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+-- keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+keymap.set("n", "ss", ":tabnext<Return>", opts)
+keymap.set("n", "sn", ":tabnext<Return>", opts)
+keymap.set("n", "sp", ":tabprev<Return>", opts)
 
 -- Move Window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sl", "<C-w>l")
+keymap.set("n", "sh", "<C-w>h", opts)
+keymap.set("n", "sj", "<C-w>j", opts)
+keymap.set("n", "sk", "<C-w>k", opts)
+keymap.set("n", "sl", "<C-w>l", opts)
 
 -- Swap Window
-keymap.set("n", "sH", "<C-w>H")
-keymap.set("n", "sJ", "<C-w>J")
-keymap.set("n", "sK", "<C-w>K")
-keymap.set("n", "sL", "<C-w>L")
+keymap.set("n", "sH", "<C-w>H", opts)
+keymap.set("n", "sJ", "<C-w>J", opts)
+keymap.set("n", "sK", "<C-w>K", opts)
+keymap.set("n", "sL", "<C-w>L", opts)
 
 -- Split Window
 
-keymap.set("n", "sw", ":new<Return>")
-keymap.set("n", "sv", ":vnew<Return>")
-keymap.set("n", "sW", ":sp<Return>")
-keymap.set("n", "sV", ":vs<Return>")
+keymap.set("n", "sw", ":new<Return>", opts)
+keymap.set("n", "sv", ":vnew<Return>", opts)
+keymap.set("n", "sW", ":sp<Return>", opts)
+keymap.set("n", "sV", ":vs<Return>", opts)
+keymap.set("n", "sd", ":tab split<Return>", opts)
 
 -- Close Window
 
-keymap.set("n", "sq", "<C-w>q")
+keymap.set("n", "sq", "<C-w>q", opts)
 
-keymap.set("n", "sc", ":tabe<Return>")
-keymap.set("n", "sf", ":e .<Return>")
+keymap.set("n", "sc", ":tabe<Return>", opts)
+-- keymap.set("n", "sf", ":e .<Return>", opts)
 
-keymap.set("n", "s1", ":tabnext 1<Return>")
-keymap.set("n", "s2", ":tabnext 2<Return>")
-keymap.set("n", "s3", ":tabnext 3<Return>")
-keymap.set("n", "s4", ":tabnext 4<Return>")
-keymap.set("n", "s5", ":tabnext 5<Return>")
-keymap.set("n", "s6", ":tabnext 6<Return>")
-keymap.set("n", "s7", ":tabnext 7<Return>")
-keymap.set("n", "s8", ":tabnext 8<Return>")
-keymap.set("n", "s9", ":tabnext 9<Return>")
+keymap.set("n", "s1", ":tabnext 1<Return>", opts)
+keymap.set("n", "s2", ":tabnext 2<Return>", opts)
+keymap.set("n", "s3", ":tabnext 3<Return>", opts)
+keymap.set("n", "s4", ":tabnext 4<Return>", opts)
+keymap.set("n", "s5", ":tabnext 5<Return>", opts)
+keymap.set("n", "s6", ":tabnext 6<Return>", opts)
+keymap.set("n", "s7", ":tabnext 7<Return>", opts)
+keymap.set("n", "s8", ":tabnext 8<Return>", opts)
+keymap.set("n", "s9", ":tabnext 9<Return>", opts)
 
-keymap.set("n", "sgf", "<C-w>gf")
-keymap.set("n", "sgF", "<C-w>gF")
+keymap.set("n", "sgf", "<C-w>gf", opts)
+keymap.set("n", "sgF", "<C-w>gF", opts)
+
+-- leader
+keymap.set("n", "<Leader>,", ":tabe ~/.config/nvim/lua/config/options.lua<Return>", opts)
+keymap.set("n", "<Leader>.", ":tabe ~/.config/nvim/lua/config/keymap.lua<Return>", opts)
+
+-- quickfix
+keymap.set("n", "<C-g>q", ":ccl<Return>", opts)
+
+-- command
+keymap.set("c", "<C-a>", "<Home>", opts)
+keymap.set("c", "<C-e>", "<End>", opts)
+keymap.set("c", "<C-f>", "<Right>", opts)
+keymap.set("c", "<C-b>", "<Left>", opts)
+keymap.set("c", "<A-a>", "<Nop>", opts)
+keymap.set("c", "<A-e>", "<Nop>", opts)
+keymap.set("c", "<A-f>", "<S-Right>", opts)
+keymap.set("c", "<A-b>", "<S-Left>", opts)
 
