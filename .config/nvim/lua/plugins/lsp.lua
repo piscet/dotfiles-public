76677b5710bb434abf94d45c9c978d2bab47f947
pisce <pisce@ymail.ne.jp>
2025-05-03 18:29:35 +0900

    add keymap

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,11 +4,25 @@ local lspconfig = require("lspconfig")
 local mason_lspconfig = require("mason-lspconfig")
 
 require("mason").setup()
-mason_lspconfig.setup()
+mason_lspconfig.setup ({
+    ensure_installed = {
+        "clangd",
+        "ast_grep",
+        "harper_ls",
+        "rust_analyzer",
+        "lua_ls",
+        "cmake",
+    }
+})
 mason_lspconfig.setup_handlers({
     function(server_name)
         lspconfig[server_name].setup({})
     end,
 })
-keymap.set('n', 'H', '<cmd>lua vim.lsp.buf.definition()<Return>')
+
+-- keymap.set('n', '<leader>g', '<cmd>:lua vim.lsp.buf.definition()<Return>')
+-- keymap.set('n', '<C-]>', '<cmd>:lua vim.lsp.buf.definition()<Return>')
+keymap.set('n', '<leader>g', vim.lsp.buf.definition)
+keymap.set('n', '<leader>s', vim.lsp.buf.references)
+-- keymap.set('n', '<leader>i', vim.lsp.buf.hover)
 
