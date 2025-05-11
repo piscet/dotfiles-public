faa493638ccd273849e0b9f677ab58bf269e2b9c
pisce <pisce@ymail.ne.jp>
2025-05-11 15:56:11 +0900

    add comment

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,10 +5,16 @@ require("config.keymap")
 -- plugin settings
 require("plugins.jetpack")
 
-require("plugins.lualine")
+-- - colorscheme
 require("plugins.colorscheme")
+
+-- - lualine
+require("plugins.lualine")
+
+-- - fzf-lua settings
 require("plugins.fzf-lua-settings")
 
+-- - lsp server, complement
 require("plugins.lsp")
 require("plugins.ddc")
 
