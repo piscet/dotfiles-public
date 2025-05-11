3d7d8e3df602f8ff30b9648ac1e5e6e1ecd35353
pisce <pisce@ymail.ne.jp>
2025-05-11 15:57:38 +0900

    fix

--- .config/nvim/lua/plugins/jetpack.lua
+++ .config/nvim/lua/plugins/jetpack.lua
@@ -1,34 +1,35 @@
 vim.cmd('packadd vim-jetpack')
 require('jetpack.packer').add {
-    'tani/vim-jetpack',
+    { 'tani/vim-jetpack' },
 
     -- colorscheme
-    'tomasr/molokai',
-    'jacoborus/tender.vim',
+    { 'tomasr/molokai' },
+    { 'jacoborus/tender.vim' },
+    { 'catppuccin/nvim', as = 'catppuccin' },
 
     -- lsp
-    'neovim/nvim-lspconfig',
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
+    { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason.nvim' , tag = 'v1.11.0', lock = true },
+    { 'williamboman/mason-lspconfig.nvim', tag = 'v1.32.0', lock = true },
 
     -- ddc
-    'vim-denops/denops.vim',
-    'Shougo/ddc.vim',
-    'Shougo/ddc-source-lsp',
-    'Shougo/ddc-source-around',
-    'Shougo/ddc-ui-native',
-    'Shougo/ddc-filter-matcher_head',
-    'Shougo/ddc-filter-sorter_rank',
+    { 'vim-denops/denops.vim' },
+    { 'Shougo/ddc.vim' },
+    { 'Shougo/ddc-source-lsp' },
+    { 'Shougo/ddc-source-around' },
+    { 'Shougo/ddc-ui-native' },
+    { 'Shougo/ddc-filter-matcher_head' },
+    { 'Shougo/ddc-filter-sorter_rank' },
 
     -- auto complement
-    'cohama/lexima.vim',
+    { 'cohama/lexima.vim' },
 
     -- status line
-    'nvim-tree/nvim-web-devicons',
-    'nvim-lualine/lualine.nvim',
+    { 'nvim-tree/nvim-web-devicons' },
+    { 'nvim-lualine/lualine.nvim' },
 
     -- fuzzy finder
-    'ibhagwan/fzf-lua'
+    { 'ibhagwan/fzf-lua' },
 }
 
 -- automatic plugin intallation on startup
